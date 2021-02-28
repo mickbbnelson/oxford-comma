00d1d38c1732894bce41a0ac37886ca2f25e39d5
Mick Nelson <mickbbnelson@gmail.com>
2021-02-28 13:54:51 -0800

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+    
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        element = array[-1]
+        array.pop
+        array.join(", ") << ", and #{element}"
+    end
+end                 
+    
